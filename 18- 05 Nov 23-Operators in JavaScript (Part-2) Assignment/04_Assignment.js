2be65d88a102e4e453cd590337372977982168aa
Shifalee Khan <78851167+Shifalee@users.noreply.github.com>
2024-01-13 15:52:16 +0530

    Add files via upload

---
+++ 18- 05 Nov 23-Operators in JavaScript (Part-2) Assignment/04_Assignment.js
@@ -0,0 +1,6 @@
+/*Use a ternary operator to check that a person is eligible to vote or not by checking his age.
+ If the age of the person is less than 18 then “You cannot vote” should be logged else “You can vote”
+  should be logged.*/
+
+const age=18;
+age<18 ? console.log("You cannot vote") : console.log("You can vote");
