118d836882be534f2fbc92b84ca3e1bea2443f3f
Shifalee Khan <78851167+Shifalee@users.noreply.github.com>
2024-01-13 15:51:58 +0530

    Add files via upload

---
+++ 18- 05 Nov 23-Operators in JavaScript (Part-2) Assignment/03_Assignment.js
@@ -0,0 +1,6 @@
+/*Use a nested ternary operator to check that a number is positive, negative or zero. You have to 
+print “positive” if the number is positive and similarly for negative and zero also.*/
+
+const ans = 0;
+const result= ans < 0 ? "Negative" : ans > 0 ? "Positive" : "Zero";
+console.log(result);
