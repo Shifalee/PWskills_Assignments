fedd7663706882a98ccf7d02f6299a0105c17b9e
Shifalee Khan <78851167+Shifalee@users.noreply.github.com>
2024-01-21 15:48:48 +0530

    Add files via upload

---
+++ 19. Condition and Loops on JavaScript/05_Assignment.js
@@ -0,0 +1,17 @@
+/*Use the while loop to print numbers from 1 to 25 in ascending and descending order.*/
+
+// Print numbers from 1 to 25 in ascending order
+console.log("Assending order 1 to 25");
+let ascendingCounter = 1;
+while (ascendingCounter <= 25) {
+  console.log(ascendingCounter);
+  ascendingCounter++;
+}
+
+// Print numbers from 25 to 1 in descending order
+console.log("Descending order 1 to 25");
+let descendingCounter = 25;
+while (descendingCounter >= 1) {
+  console.log(descendingCounter);
+  descendingCounter--;
+}
