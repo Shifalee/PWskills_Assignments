b0cbff3956860b132cc88a30828c5cf8c8257cc2
Shifalee Khan <78851167+Shifalee@users.noreply.github.com>
2024-01-21 15:48:30 +0530

    Add files via upload

---
+++ 19. Condition and Loops on JavaScript/04_Assignment.js
@@ -0,0 +1,15 @@
+/*Generate numbers between any 2 given numbers.
+Ex-const num1=10;
+const num2=25;
+output=11,12,13,...,25*/
+
+const num1=10;
+const num2=25;
+let i;
+
+for(i=num1;i<=num2;i++){
+    if(i===num1)
+    continue;
+    console.log(i);
+    
+}
